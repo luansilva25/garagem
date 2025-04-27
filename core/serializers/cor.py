863a3aec189890120c6c85a7286512e40edb74e1
luansilva25 <luansilva250807@gmail.com>
2025-04-27 11:44:33 -0300

    feat: Serialização da model Cor

---
+++ core/serializers/cor.py
@@ -0,0 +1,7 @@
+from core.models import Cor
+from rest_framework.serializers import ModelSerializer
+
+class CorSerializer(ModelSerializer):
+    class Meta: 
+        model = Cor
+        fields = '__all__'
