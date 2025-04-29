dd23eb9adb8fc1e130923f6c854de7aed1374f09
luansilva25 <luansilva250807@gmail.com>
2025-04-29 16:48:43 -0300

    REFACTOR: refactor related names fields

--- core/models/veiculo.py
+++ core/models/veiculo.py
@@ -4,9 +4,9 @@ from core.models import Cor, Modelo, Acessorio
 class Veiculo(models.Model):
     ano = models.IntegerField(default=0, blank=True, null=True)
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0, blank=True, null=True)
-    acessorio = models.ManyToManyField(Acessorio, blank=True, related_name='veiculo')
-    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, null=True, blank=True, related_name='veiculo')
-    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, null=True, blank=True, related_name='veiculo')
+    acessorio = models.ManyToManyField(Acessorio, blank=True, related_name='veiculos')
+    cor = models.ForeignKey(Cor, on_delete=models.PROTECT, null=True, blank=True, related_name='veiculos')
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, null=True, blank=True, related_name='veiculos')
 
     def __str__(self):
         return f'({self.id}) - {self.modelo.nome} - {self.cor.nome} - {self.ano}'
